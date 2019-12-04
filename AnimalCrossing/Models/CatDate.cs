161b038be261bee9a6a310b4e60fe68184ae195a
Gustav Babbelap <killercomp2.0@Gustavs-MacBook-Air.local>
2019-12-04 09:48:22 +0100

    added catdating and catReview model classes

---
+++ AnimalCrossing/Models/CatDate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AnimalCrossing.Models
+{
+    public class CatDate
+    {
+        public CatDate()
+        {
+        }
+    }
+}
---
+++ AnimalCrossing/Models/CatReview.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AnimalCrossing.Models
+{
+    public class CatReview
+    {
+        public CatReview()
+        {
+        }
+    }
+}
